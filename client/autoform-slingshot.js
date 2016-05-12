8085c2993a643fd77eec8b38b34744d46c49518a
Steve <oohaysmlm@gmail.com>
2016-05-12 13:05:53 -0700

    Initial Commit

---
+++ client/autoform-slingshot.js
@@ -0,0 +1,116 @@
+/**
+ * Created by stevenkehoe on 4/22/16.
+ */
+
+AutoForm.addInputType('slingshotUpload', {
+    template: "afSlingshot",
+    valueOut: function(){
+        var fileRecord = Session.get(this.attr('data-schema-key') + "_fileRecord");
+        return fileRecord;
+
+    }
+});
+
+let template;
+
+let _getFileFromInput = ( event ) => event.target.files[0];
+
+let _setPlaceholderText = ( string = "Click or Drag a File Here to Upload" ) => {
+    template.find( ".alert span" ).innerText = string;
+};
+
+let _uploadFileToAmazon = (file, directive, name, schemaKey) => {
+    const uploader = new Slingshot.Upload( directive );
+
+    uploader.send( file, ( error, url ) => {
+        if ( error ) {
+            alert( error.message);
+            _setPlaceholderText();
+        } else {
+
+            let img = new Image();
+            img.onload = function() {
+                var fileRecord = Session.get(schemaKey + "_fileRecord");
+                fileRecord[name] = {
+                    src: url,
+                    name: file.name,
+                    size: file.size,
+                    type: file.type,
+                    directive: directive,
+                    width: this.width,
+                    height: this.height
+                };
+                Session.set(schemaKey + "_fileRecord", fileRecord);
+            }
+            img.src = url;
+
+
+        }
+    });
+};
+
+let uploadToAmazonS3 = ( options ) => {
+    template = options.template;
+    let file = _getFileFromInput( options.event );
+
+    _setPlaceholderText( `Uploading ${file.name}...` );
+    if(options.onBeforeUpload){
+        options.onBeforeUpload(file, function(file){
+            _uploadFileToAmazon(file, options.directive, options.name,  options.schemaKey);
+        });
+    }
+    else {
+        _uploadFileToAmazon(file, options.directive, options.name, options.schemaKey);
+    }
+
+
+};
+
+Template.afSlingshot.events({
+    'change input[type="file"]' ( event, template ) {
+        /*
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(fileLoadEvent) {
+            Meteor.call('largeThumbImage', file, reader.result);
+        };
+        reader.readAsBinaryString(file);
+         */
+        var directives = this.atts.slingshotdirective;
+        for(var key in directives){
+            if (directives.hasOwnProperty(key) && (typeof directives[key] === "object")) {
+                uploadToAmazonS3( { event: event, template: template, directive: directives[key].directive, name:key, onBeforeUpload: directives[key].onBeforeUpload, schemaKey:this.atts['data-schema-key'] } );
+            }
+            else if(directives.hasOwnProperty(key) && (typeof directives[key] === "string")){
+                uploadToAmazonS3( { event: event, template: template, directive: directives[key], name:key, schemaKey:this.atts['data-schema-key'] } );
+            }
+        }
+
+    },
+    'click .file-upload-clear' : function(e) {
+        Session.set(this.atts['data-schema-key'] + "_fileRecord", {});
+    }
+});
+
+Template.afSlingshot.helpers({
+   schemaKey: function(){
+       return this.atts['data-schema-key'];
+   },
+    thumbnail: function(){
+        var fileRecord = Session.get(this.atts['data-schema-key'] + "_fileRecord");
+        if(fileRecord[this.atts.thumbnail]){
+            return fileRecord[this.atts.thumbnail].src;
+        }
+    }
+});
+
+
+
+Template.afSlingshot.onCreated(function(){
+    if(this.data.value){
+        Session.set(this.data.atts['data-schema-key'] + "_fileRecord", this.data.value);
+    }
+    else {
+        Session.set(this.data.atts['data-schema-key'] + "_fileRecord", {});
+    }
+});
---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'skehoe1989:autoform-slingshot',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'An autoform input for edgee:slingshot',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use('session')
+  api.use('blaze-html-templates');
+  api.use('aldeed:autoform@5.7.1');
+  api.use('edgee:slingshot');
+  api.addFiles('client/autoform-slingshot.html', 'client');
+  api.addFiles('client/autoform-slingshot.js', 'client');
+});
+
