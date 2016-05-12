57844abb6f6fdc3cdbddce39e9beaf0191714aca
Steve <oohaysmlm@gmail.com>
2016-05-12 13:49:26 -0700

    Added git repo to package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'An autoform input for edgee:slingshot',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/oohaysmlm/skehoe1989-autoform-slingshot',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
